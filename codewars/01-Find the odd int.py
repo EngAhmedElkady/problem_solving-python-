dcb2efc5d2c1f99632e7595c2dd96f52aaaaad99
EngAhmedElkady <ahelkady110@gmail.com>
2021-08-15 14:41:43 +0200

    second commit

---
+++ codewars/01-Find the odd int.py
@@ -0,0 +1,24 @@
+m=dict()
+
+def find_it(seq):
+    global m
+    
+    for i in seq:m[i]=0
+    for i in seq:
+        m[i]+=1
+
+    for i in m:
+        x=m.get(i)
+        if x%2!=0:
+            return x
+
+    
+    return None
+
+
+
+print(find_it([1,1,2,2,2,2,3,3,3]))
+# ans==> 3
+
+
+
